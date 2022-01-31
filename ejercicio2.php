994de71cf6cee9a05b655dd65a0e980fb60b392e
aprouco <adripenasrouco@gmail.com>
2022-01-31 08:56:58 +0100

    Ejercicio2

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,6 +1,5 @@
 <?php
     header('Content-Type: text/html; charset=UTF-8');
-    print_r($_REQUEST);
     $euros = $_REQUEST["euros"];
     $total = $euros * 166.3860;
     echo "<br/>$euros euros equivalen a: ".$total." pesetas";
